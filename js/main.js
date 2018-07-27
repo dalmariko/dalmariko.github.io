a77c71e2fcffebd9fd0a0a5e340890ebea0f4e26
dalmariko <dalmarikocamp@gmail.com>
2018-07-27 07:18:59 +0300

    addet all in too the site

--- js/main.js
+++ js/main.js
@@ -9,13 +9,47 @@ function myW(){
 if ($(window).width()<768) {
 			$('.headerFerst .headerLeft').appendTo('.mobileMenu');
 			$('.headerFerst .headerRight').appendTo('.mobileMenu');
+			$('.headerFerst .toggleBtn').appendTo('.mobileMenu');
 }else {
 			$('.mobileMenu .headerLeft').appendTo('.headerFerst .myRow');
 			$('.mobileMenu .headerRight').appendTo('.headerFerst .myRow');
+			$('.mobileMenu .toggleBtn').appendTo('.headerFerst .myRow');
 	  }
     };
 
 $(window).on('load resize',myW);
+
+
+$('.toggleBtn').on('click',function(){
+$('.headerBottom').slideToggle(); 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
