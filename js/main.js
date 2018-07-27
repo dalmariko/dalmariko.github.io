a09fedf3fe9d2949ee63b3fd6a37b1fe5431b0ef
dalmariko <dalmarikocamp@gmail.com>
2018-07-27 05:58:32 +0300

    track screen with jQuery

--- js/main.js
+++ js/main.js
@@ -3,4 +3,19 @@ $(function(){
   arrows:false,
   dots: true,
 	});
+
+
+function myW(){
+if ($(window).width()<768) {
+			$('.headerFerst .headerLeft').appendTo('.mobileMenu');
+			$('.headerFerst .headerRight').appendTo('.mobileMenu');
+}else {
+			$('.mobileMenu .headerLeft').appendTo('.headerFerst .myRow');
+			$('.mobileMenu .headerRight').appendTo('.headerFerst .myRow');
+	  }
+    };
+
+$(window).on('load resize',myW);
+
+
 });
