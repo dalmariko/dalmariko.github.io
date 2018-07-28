ab7585ec077de43bbcb808dad302f5a2430c8367
dalmariko <dalmarikocamp@gmail.com>
2018-07-28 06:43:32 +0300

    last rerayp prohopse

--- js/main.js
+++ js/main.js
@@ -9,11 +9,9 @@ function myW(){
 if ($(window).width()<768) {
 			$('.headerFerst .headerLeft').appendTo('.mobileMenu');
 			$('.headerFerst .headerRight').appendTo('.mobileMenu');
-			$('.headerFerst .toggleBtn').appendTo('.mobileMenu');
 }else {
 			$('.mobileMenu .headerLeft').appendTo('.headerFerst .myRow');
 			$('.mobileMenu .headerRight').appendTo('.headerFerst .myRow');
-			$('.mobileMenu .toggleBtn').appendTo('.headerFerst .myRow');
 	  }
     };
 
