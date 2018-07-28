da7e033cc2dcf065d2dc8110d618e598579e92e5
Dimka <manager@perilman.com.ua>
2018-07-28 17:21:42 +0300

    bottom menu toggle

--- js/main.js
+++ js/main.js
@@ -6,21 +6,30 @@ $(function(){
 
 
 function myW(){
+
+
 if ($(window).width()<768) {
 			$('.headerFerst .headerLeft').appendTo('.mobileMenu');
 			$('.headerFerst .headerRight').appendTo('.mobileMenu');
+			// $('.headerSecond .headerLeft').appendTo('.mobileMenu');
+			// $('.headerSecond .headerRight').appendTo('.mobileMenu');
 }else {
 			$('.mobileMenu .headerLeft').appendTo('.headerFerst .myRow');
 			$('.mobileMenu .headerRight').appendTo('.headerFerst .myRow');
+			// $('.mobileMenu .headerLeft').appendTo('.headerSecond .myRow');
+			// $('.mobileMenu .headerRight').appendTo('.headerSecond .myRow');
 	  }
     };
 
+
+
+	$('.toggleBtn').on('click',function(){
+		$('.headerBottom').slideToggle();
+	});
+
+	
+
 $(window).on('load resize',myW);
-
-
-$('.toggleBtn').on('click',function(){
-$('.headerBottom').slideToggle(); 
-});
 
 
 
