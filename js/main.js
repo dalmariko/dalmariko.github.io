6f6c6d083ba34aa0dff8dc0490e0091943ae23d5
dalmariko <dalmarikocamp@gmail.com>
2018-11-11 14:15:50 +0200

    no scroll before slide r no click end

--- js/main.js
+++ js/main.js
@@ -1,34 +1,87 @@
-$(function(){
-	$('.main-slider').slick({
-  arrows:false,
-  dots: true,
-	});
+$(function () {
+
+    $('.main-slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 
 
-	  
+    $(window).on('resize', function () {
+        if ($(window).width() < 768) {
+            $('#hsr').appendTo('.mobileMenuRight');
+            $('#hfl').appendTo('.mobileMenuLeft');
+            $('#hfr').appendTo('.mobileMenuLeft');
+            $('.headerBottom').slideUp();
+        } else {
+            $('#hfl').appendTo('.headerFerst  .myRow');
+            $('#hfr').appendTo('.headerFerst  .myRow');
+            $('#hsr').appendTo('.headerSecond  .myRow');
+            $('.headerBottom').slideDown();
+        }
+    });
+    $(window).trigger('resize');
+    $('.toggleBtn').on('click', function () {
+        $('.headerBottom').slideToggle();
+    });
+
+
+//TODO Запретить скролл
+//TODO Считать колличество слайдов
+//TODO По истичению счетчика включить скрол
+
+
+    const slickSlider = document.querySelector('.slick-slider');
+
+    /*
+    window.addEventListener('scroll', e => {
+        // window.pageYOffset=0;
+        // document.body.overflow = 'hidden';
+        // document.body.style.overflow = 'auto';
+
+        // window.scrollTo(0, 0); //рабочий вариант
+
+    });
+    */
+
+    let maxSliders = document.querySelectorAll('[data-slick-index]').length - 1;
+    console.log(maxSliders);
+
+
+    window.addEventListener('load', () => {
+        document.body.style.overflow = 'hidden';
+    });
 
 
 
-$(window).on('resize', function(){
-if ($(window).width()<768) {
-			$('#hsr').appendTo('.mobileMenuRight');
-			$('#hfl').appendTo('.mobileMenuLeft');
-			$('#hfr').appendTo('.mobileMenuLeft');
-			$('.headerBottom').slideUp();
-}else {
-			$('#hfl').appendTo('.headerFerst  .myRow');
-			$('#hfr').appendTo('.headerFerst  .myRow');
-			$('#hsr').appendTo('.headerSecond  .myRow');
-			$('.headerBottom').slideDown();
-	  }
+
+    let  options = {
+        attributeFilter:['class']
+        };
+
+   let activeSlider = document.querySelector('.slick-active');
+
+
+    activeSlider.addEventListener('movemouse', e => {
+        let id = e.target.closest("[data-slick-index]").dataset.slickIndex;
+
+        console.log(maxSliders, id * 1);
+
+        if (maxSliders === id * 1) {
+            document.body.style.overflow = 'auto';
+            console.log('last slider')
+        }
+    });
+
+
 });
 
-$(window).trigger('resize');
 
-
-
-$('.toggleBtn').on('click',function(){
-		$('.headerBottom').slideToggle();
+window.addEventListener('scroll', e => {
+    var scrolled = window.pageYOffset = 0 || document.documentElement.scrollTop;
+    document.getElementById('showScroll').innerHTML = scrolled + 'px';
 });
 
 
@@ -36,20 +89,3 @@ $('.toggleBtn').on('click',function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
